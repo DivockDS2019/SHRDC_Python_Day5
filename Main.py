ce2567d269c35695fabad79dd475d4ccf1b8078f
DivockDS2019 <danielsawujen2003@gmail.com>
2026-03-05 15:15:50 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import io
+import matplotlib.pyplot as plt
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import A4
+from datetime import datetime
+import os
+
+
+
+# -------------------------------------------------
+# PAGE CONFIG
+# -------------------------------------------------
+st.set_page_config(page_title="Covid-19 Dashboard", layout="wide")
+st.title("Covid-19 (Data Visualisation + PDF Export)")
+
+df = pd.read_csv('dataset.csv')
+dictionary = pd.read_excel('data_dictionary.xlsx')
+
+
+
+
+
